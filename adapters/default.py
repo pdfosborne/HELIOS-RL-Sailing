e971c3b3a8818e217a6fbb82c8708ee79648d513
Philip Osborne <kr015604@gmail.com>
2023-07-03 12:02:16 +0100

    Initial setup of the Sailing engine

--- adapters/default.py
+++ adapters/default.py
@@ -1,13 +1,36 @@
 from typing import Dict, List
 import pandas as pd
+import numpy as np
 import torch
 from torch import Tensor
 # StateAdapter includes static methods for adapters
 from helios_rl.encoders.poss_state_encoded import StateEncoder
 
 class DefaultAdapter:
-    _cached_state_idx: Dict[str, int] = dict()
 
+    # ------ Static Methods ---------------------------------------
+    # - Defined by simulator source https://github.com/PPierzc/ai-learns-to-sail/blob/master/tasks/channel.py
+    @staticmethod
+    def angle_to_state(angle):
+        return int(30 * ((angle + np.pi) / (2 * np.pi) % 1))  # Discretization of the angle space
+    
+    @staticmethod
+    def x_to_state(x):
+        return int(40 * ((x + -10) / 20))  # Discretization of the x space
+    
+    @staticmethod
+    def state_discretizer(state):
+        x = float(state.split('_')[0])
+        x_state = DefaultAdapter.x_to_state(x)
+
+        angle = float(state.split('_')[1])
+        angle_state = DefaultAdapter.angle_to_state(angle)
+
+        state_out = str(x_state)+'_'+str(angle_state)
+        return state_out
+    # -------------------------------------------------------------
+
+    _cached_state_idx: Dict[str, int] = dict()
     def __init__(self):
         # TODO: Update this based on the current problem, each requires preset knowledge of all possible states/actions/objects
         # - Possible Atates
@@ -16,12 +39,23 @@ class DefaultAdapter:
         # - Possible Objects
     
         # Initialise encoder based on all possilbe env states
-        all_possible_states = [i for i in range(4*4)]
+        all_possible_x = [i*-1 for i in range(40)]
+        all_possible_angle = [i for i in range(30)]
+        # Need an index that preserves the identity of both the x and angle values
+        all_possible_states = []
+        for x_ind in all_possible_x:
+            for angle_ind in all_possible_angle:
+                index = str(x_ind)+'_'+str(angle_ind)
+                all_possible_states.append(index)
+        # Input to pre-built possible state encoder
         self.encoder = StateEncoder(all_possible_states)
+    
     
     def adapter(self, state:any, legal_moves:list = None, episode_action_history:list = None, encode:bool = True, indexed: bool = False) -> Tensor:
         """ Use Language name for every piece name for current board position """
-       
+        
+        state = DefaultAdapter.state_discretizer(state)
+
         # Encode to Tensor for agents
         if encode:
             state_encoded = self.encoder.encode(state=state)
--- environment/engine.py
+++ environment/engine.py
@@ -1,3 +1,8 @@
+# Sailing Simulator
+# - https://github.com/topics/sailing-simulator
+# - Simple sailing simulator from https://github.com/PPierzc/ai-learns-to-sail
+#   - https://github.com/PPierzc/ai-learns-to-sail/blob/master/tasks/channel.py
+import numpy as np
 
 class Engine:
     """Defines the environment function from the generator engine.
@@ -9,10 +14,25 @@ class Engine:
     def __init__(self) -> None:
         """Initialize Engine"""
         self.Environment = "Engine Initialization"
-        
+
+    # --------------------------
+    # Defined functions used by engine source
+    @staticmethod
+    def vel(theta, theta_0=0, theta_dead=np.pi / 12):
+        return 1 - np.exp(-(theta - theta_0) ** 2 / theta_dead)
+    
+    @staticmethod
+    def rew(theta, theta_0=0, theta_dead=np.pi / 12):
+        return Engine.vel(theta, theta_0, theta_dead) * np.cos(theta)
+    # --------------------------
+
     def reset(self):
         """Fully reset the environment."""
-        obs, _ = self.Environment.reset()
+        #obs, _ = self.Environment.reset()
+        self.x = np.random.uniform(-9.9, 9.9)
+        self.angle = 0  # always start with angle 0
+        self.y = 0
+        obs = str(self.x)+'_'+str(self.angle)
         return obs
 
     
@@ -22,11 +42,35 @@ class Engine:
         if (state=="ENV_RESET")|(action=="ENV_RESET"):
             self.reset()
             
-        obs, reward, terminated = self.Environment.step(action)
+        a = [-0.1, 0.1][action]
+
+        # Observation space
+        self.x += (Engine.vel(self.angle + a) * np.sin(self.angle + a))
+        self.y += (Engine.vel(self.angle + a) * np.cos(self.angle + a))
+        self.angle += a
+        obs = str(self.x)+'_'+str(self.angle)
+        
+        # Reward signal
+        reward = Engine.rew(self.angle)
+
+        # Termination signal
+        # - Source: Terminal only on hitting piers/walls, otherwise continues to action limiit
+        # - Update: Add terminal state if y > 25 (or another arbirary value)
+        if np.abs(self.x)>10:
+            reward = -1
+            terminated = True
+        elif np.abs(self.y)>25:
+            reward = 1
+            terminated = True
+        else:
+            terminated = False
+        
         return obs, reward, terminated
 
     def legal_move_generator(self, obs:any=None):
         """Define legal moves at each position"""
-        legal_moves = self.Environment.legal_moves(obs)
+        # Action space: [0,1] for turn slightly left or right
+        # - Kept as binary but might be better as continuous [-0.1, 0.1]
+        legal_moves = [0, 1]
         return legal_moves
 
